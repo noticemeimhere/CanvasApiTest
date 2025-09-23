124fff2cba3735f655ac1f2324dc6871d80f9569
Mitya Chirkov <noticemeimherebuisness@gmail.com>
2025-09-23 14:17:17 -0700

    more grade testing

--- main.py
+++ main.py
@@ -17,24 +17,27 @@ my_enrollment = enrollments[0]
 print(my_enrollment.grades['current_score'])
 print(my_enrollment.grades['current_grade'])
 
+courses = canvas.get_courses()
+
+listofusers = []
+
+for course in courses:
+    print(f"{course.name}: {course.id}")
+    for user in course.get_users(enrollment_type=['student']):
+        print(f"{user.name}: {user.id}")
+        listofusers.append(user.id)
+    enrollments = course.get_enrollments(user_id=216, type=["StudentEnrollment"])
+    my_enrollment = enrollments[0]
+
+    print(f"Grade: {my_enrollment.grades['current_score']}%")
+    print(my_enrollment.grades['current_grade'])
 
 
+newlist = list(set(listofusers))
 
-# courses = canvas.get_courses()
+newlist.sort()
 
-# listofusers = []
-
-# for course in courses:
-#     print(f"{course.name}: {course.id}")
-#     for user in course.get_users(enrollment_type=['student']):
-#         listofusers.append(user.id)
-
-
-# newlist = list(set(listofusers))
-
-# newlist.sort()
-
-# print(newlist)
+print(newlist)
 
 
 
