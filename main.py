26fc055684e4f062d884f0578a7f9d2846f8f444
Mitya Chirkov <noticemeimherebuisness@gmail.com>
2025-10-27 10:14:33 -0700

    worlds greatest gpa checker

--- main.py
+++ main.py
@@ -17,10 +17,12 @@ my_enrollment = enrollments[0]
 courses = canvas.get_courses()
 user = course.get_users
 
-for course in courses:
-    users = course.get_users()
-    for user in users:
-        print(user)
+# for course in courses:
+#     users = course.get_users()
+#     for user in users:
+#         print(user)
+
+
 
 
 #returns a gpa score from a grade
@@ -29,7 +31,37 @@ def get_gpa_from_grade(grade):
         gpa = 4.0
     elif grade >= 90:
         gpa = 3.7
+    elif grade >= 87:
+        gpa = 3.3
+    elif grade >= 83:
+        gpa = 3
+    elif grade >= 80:
+        gpa = 2.7
+    elif grade >= 77:
+        gpa = 2.3
+    elif grade >= 73:
+        gpa = 2
+    elif grade >= 70:
+        gpa = 1.7
+    elif grade >= 67:
+        gpa = 1.3
+    elif grade >= 60:
+        gpa = 1
+    else:
+        gpa = 0
     return gpa
+
+print(get_gpa_from_grade(93))
+print(get_gpa_from_grade(90))
+print(get_gpa_from_grade(87))
+print(get_gpa_from_grade(83))
+print(get_gpa_from_grade(80))
+print(get_gpa_from_grade(77))
+print(get_gpa_from_grade(73))
+print(get_gpa_from_grade(70))
+print(get_gpa_from_grade(67))
+print(get_gpa_from_grade(60))
+print(get_gpa_from_grade(59))
 
 for course in courses:
     
