7b3e804f45dc8874dd937e401491bc3c5ac13b1e
Mitya Chirkov <noticemeimherebuisness@gmail.com>
2025-10-28 09:08:23 -0700

    asda

--- main.py
+++ main.py
@@ -11,16 +11,17 @@ canvas = Canvas(API_URL,API_KEY)
 
 course = canvas.get_course(144)
 
-enrollments = course.get_enrollments(user_id=216, type=["StudentEnrollment"])
-my_enrollment = enrollments[0]
-
 courses = canvas.get_courses()
-user = course.get_users
+print(courses)
 
-# for course in courses:
-#     users = course.get_users()
-#     for user in users:
-#         print(user)
+gpalist = []
+
+print(canvas.get_user(216))
+
+for course in courses:
+    users = course.get_users()
+    for user in users:
+        print(user)
 
 
 
@@ -51,17 +52,6 @@ def get_gpa_from_grade(grade):
         gpa = 0
     return gpa
 
-print(get_gpa_from_grade(93))
-print(get_gpa_from_grade(90))
-print(get_gpa_from_grade(87))
-print(get_gpa_from_grade(83))
-print(get_gpa_from_grade(80))
-print(get_gpa_from_grade(77))
-print(get_gpa_from_grade(73))
-print(get_gpa_from_grade(70))
-print(get_gpa_from_grade(67))
-print(get_gpa_from_grade(60))
-print(get_gpa_from_grade(59))
 
 for course in courses:
     
@@ -75,4 +65,6 @@ for course in courses:
         print(f"\n{course.name}: {course.id}")
         print(f"Grade: {my_enrollment.grades['current_score']}%")
         print(get_gpa_from_grade(grade))
+        gpalist.append(get_gpa_from_grade(grade))
         
+print(round(sum(gpalist)/7,2))
